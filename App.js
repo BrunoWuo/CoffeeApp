a1856b1fea59d258e9b0733c413684fcd69b2ac2
Bruno Wuo <104470819+BrunoWuo@users.noreply.github.com>
2024-05-12 19:39:49 -0300

    Estrutura Base

---
+++ App.js
@@ -0,0 +1,8 @@
+import TabNavigator from './src/routes/TabNavigator';
+
+export default function App() {
+  return (
+    <TabNavigator/>
+  );
+}
+
---
+++ src/data/dbcafe.js
@@ -0,0 +1,12 @@
+const tiposCafes = [
+    {
+      "id": 1,
+      "titulo": "Espresso",
+      "imagem": "http://localhost:8081/assets/img/espresso.png",
+      "ingredientes": [
+        "Café Especial",
+        "Água Filtrada"
+      ],
+      "descricao": "O café espresso é um método de preparo de café originário da Itália. Envolve a passagem de água quente sob alta pressão através de grãos de café finamente moídos, resultando em uma xícara de café concentrada, encorpada e rica em sabor."
+    },
+]
