6c226f3774ae1445eab7e96ad28d927b1c728824
Bruno Wuo <104470819+BrunoWuo@users.noreply.github.com>
2024-05-12 21:34:32 -0300

    Update TabNavigator.js

--- src/routes/TabNavigator.js
+++ src/routes/TabNavigator.js
@@ -1,14 +1,13 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Initial from "../screens/Initial"
-import Home from "../screens/Home"
-import Details from "../screens/Details"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import Home from "../screens/Home";
+import Details from "../screens/Details";
 
 const Tab = createBottomTabNavigator();
 
 export default function TabNavigator() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Initial" component={Initial} />
       <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Details" component={Details} />
     </Tab.Navigator>
