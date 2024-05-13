986f9edb880a29e91159da109299c139d1ed378f
Bruno Wuo <104470819+BrunoWuo@users.noreply.github.com>
2024-05-12 21:26:41 -0300

    Update StackNavigator.js

--- src/routes/StackNavigator.js
+++ src/routes/StackNavigator.js
@@ -1,1 +1,11 @@
 
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function StackNavigator() {
+  return (
+    <View>
+      <Text>StackNavigator</Text>
+    </View>
+  )
+}
