97f3f10c040135614242156c1669eb3af22d1a06
Bruno Wuo <104470819+BrunoWuo@users.noreply.github.com>
2024-05-12 21:34:11 -0300

    Create StackNavigator.js

---
+++ src/routes/StackNavigator.js
@@ -0,0 +1,19 @@
+import { createStackNavigator } from "@react-navigation/stack";
+
+import TabNavigator from "./TabNavigator";
+import Initial from "../screens/Initial";
+
+const Stack = createStackNavigator();
+
+export default function StackNavigator() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name="Initial" component={Initial} />
+      <Stack.Screen name="TabNavigator" component={TabNavigator} />
+    </Stack.Navigator>
+  );
+}
